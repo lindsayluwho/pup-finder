2240061654d4e2a277e71da2091c939d030c36f5
Lindsay Brown <lindsayluwho@gmail.com>
2018-01-19 15:22:10 -0500

    set up pups object structure, set up htmlRoutes, added pseudocode in pups.js and comments in server.js

---
+++ app/data/pups.js
@@ -0,0 +1,18 @@
+//array of pups objects!
+var pups = [
+{
+	name:"",
+	photo:"",
+	scores: [];
+},
+];
+
+//placeholder for user input from survey
+var user = {
+	scores: [];
+}
+
+//compare user's input scores with scores of each pup and return closest match
+function compareScores(){
+
+};
--- server.js
+++ server.js
@@ -4,11 +4,23 @@ var path = require("path");
 var apiRoutes = require ("/app/routing/apiRoutes");
 var htmlRoutes = require("/app/routing/htmlRoutes");
 
-
+//call Express, set up port
 var app = express();
+var PORT = process.env.PORT || 3000;
 
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//set up routes
 apiRoutes(app);
 htmlRoutes(app);
+
+//listener
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
+
 
 
 
